f9d0ebd6715beda441511fe5597f1f9e011c17b3
Millat <Millatmzere72753@gamil.com>
2024-07-16 07:28:45 +0300

    test_1

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById("icon").addEventListener("click", function () {
+  document.getElementById("myDiv").classList.toggle("menu");
+  document.getElementById("icon-color").classList.toggle("menu--icon");
+});
