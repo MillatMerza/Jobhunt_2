e4b00dd0f10fb96847380b75010716b02e9738e1
Millat <millatmzere72753@gmail.com>
2024-09-29 02:33:57 +0300

    test_1

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ document.getElementById("icon").addEventListener("click", function () {
   document.getElementById("myDiv").classList.toggle("menu");
   document.getElementById("icon-color").classList.toggle("menu--icon");
 });
+
+
+ 
