bdff99f839251b28cf05b65570af2a65e9200b18
Millat <millatmzere72753@gmail.com>
2024-09-29 03:07:09 +0300

    test_3

--- main.js
+++ main.js
@@ -3,21 +3,3 @@ document.getElementById("icon").addEventListener("click", function () {
   document.getElementById("icon-color").classList.toggle("menu--icon");
 });
 
-document
-  .getElementById("start-camera-btn")
-  .addEventListener("click", function () {
-    // Access the camera with the `facingMode: environment` to specify the back camera
-    navigator.mediaDevices
-      .getUserMedia({ video: { facingMode: { exact: "environment" } } })
-      .then(function (stream) {
-        const video = document.getElementById("camera");
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch(function (error) {
-        console.error("Error accessing the camera: ", error);
-        alert(
-          "Unable to access the back camera. Please check your device settings."
-        );
-      });
-  });
